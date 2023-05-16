20fa68a596e6b567e9ebb146f27548ded5ba8867
MardonZarifjonov <m.zarifjonov16@gmail.com>
2023-05-16 11:00:01 +0500

    project configuration

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,14 +1,87 @@
 module.exports = {
-  env: { browser: true, es2020: true },
+  root: true,
+  env: {
+    browser: true,
+    es2021: true,
+    jest: true,
+    node: true,
+  },
   extends: [
+    'airbnb',
+    'airbnb-typescript',
     'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
     'plugin:react-hooks/recommended',
+    'plugin:react/jsx-runtime',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:jsx-a11y/recommended',
+    'plugin:prettier/recommended',
+    'eslint-config-prettier',
   ],
   parser: '@typescript-eslint/parser',
-  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
-  plugins: ['react-refresh'],
-  rules: {
-    'react-refresh/only-export-components': 'warn',
+  parserOptions: {
+    project: './tsconfig.json',
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
   },
-}
+  plugins: [
+    'react',
+    'react-hooks',
+    '@typescript-eslint',
+    'import',
+    'react-refresh',
+  ],
+  rules: {
+    'import/no-extraneous-dependencies': 'off',
+    'react-refresh/only-export-components': 'warn',
+    'consistent-return': ['off'],
+    'no-console': [
+      'warn',
+      {
+        allow: ['error', 'warn'],
+      },
+    ],
+    'no-param-reassign': ['error', { props: false }],
+    'no-multiple-empty-lines': ['error', { max: 2 }],
+    'no-underscore-dangle': ['off'],
+    camelcase: [
+      'error',
+      {
+        properties: 'never',
+        ignoreDestructuring: false,
+        ignoreImports: true,
+      },
+    ],
+    '@typescript-eslint/no-unused-vars': 'off',
+    '@typescript-eslint/comma-dangle': 'off',
+
+    '@typescript-eslint/no-unused-expressions': [
+      'error',
+      {
+        allowTernary: true,
+        allowShortCircuit: true,
+      },
+    ],
+    '@typescript-eslint/quotes': 'off',
+    'react/react-in-jsx-scope': 'off',
+    'react/prop-types': 0,
+    'react/require-default-props': 'off',
+    'react/jsx-props-no-spreading': 'off',
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
+    'react/jsx-uses-react': 'error',
+    'react/jsx-uses-vars': 'error',
+    'react/button-has-type': 'off',
+    'import/extensions': 'off',
+    'import/namespace': 0,
+    'import/no-unresolved': 'off',
+    'import/prefer-default-export': 'off',
+    'padding-line-between-statements': [
+      'error',
+      { blankLine: 'always', prev: '*', next: 'return' },
+    ],
+  },
+};
